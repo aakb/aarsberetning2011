ba9e7e0b42b15e9182e56904ee99038dab4e9724
Brian Jensen <mail@magnify.dk>
2012-04-26 14:59:06 +0200

    Added rgbapng for cross-browser transparent png support

--- themes/aarsberetning2011/config.rb
+++ themes/aarsberetning2011/config.rb
@@ -10,6 +10,10 @@ javascripts_dir = "scripts"
 output_style = :expanded
 environment = :development
 
+require "rubygems"
+require "chunky_png"
+require "rgbapng"
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
